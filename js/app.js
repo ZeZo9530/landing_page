ada426d7a95ca420691fe1d1e25a1a86bbc7847f
zezoz9530 <91164869+ZeZo9530@users.noreply.github.com>
2021-11-04 14:14:54 +0200

    updated

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@
 //function to add item list 
 
   function sectionAdder (x){
-    for (var i =1; i <= x; i++){
+    for (let i =1; i <= x; i++){
       const newListItem = document.createElement('li');
 
       //set ID and Class for later
@@ -63,46 +63,34 @@
   }
 
   //build the nav
-  var navBar = document.getElementById('navbar__list');
+  let navBar = document.getElementById('navbar__list');
   sectionAdder(4);
 
-// Add class 'active' to section when near top of viewport: done above.
+// Add class 'active' to section when near top of viewport
 
 
 // Scroll to anchor ID using scrollTO event
-
-/* i tried this but didnt work , it goes throw every itemlist and section individually
-
-for (var i = 1; i <= 4 ; i++){
-  var item = "sec"+i;
-  var section = "section"+i;
-  document.getElementById(item).addEventListener('click',function (){
-    document.getElementById(section).scrollIntoView({behavior:"smooth"})
-  })
-}
-
-*/
 
 // on click goes to the section
 
 document.getElementById('sec1').addEventListener('click',function (){
   document.getElementById('section1').scrollIntoView({behavior:"smooth"})
-  activeSection(document.getElementById('section1'));
+  
 })
 
 document.getElementById('sec2').addEventListener('click',function (){
   document.getElementById('section2').scrollIntoView({behavior:"smooth"})
-  activeSection(document.getElementById('section2'));
+  
 })
 
 document.getElementById('sec3').addEventListener('click',function (){
   document.getElementById('section3').scrollIntoView({behavior:"smooth"})
-  activeSection(document.getElementById('section3'));
+  
 })
 
 document.getElementById('sec4').addEventListener('click',function (){
   document.getElementById('section4').scrollIntoView({behavior:"smooth"})
-  activeSection(document.getElementById('section4'));
+  
 })
 
 
@@ -118,7 +106,7 @@ function isInViewport(elm) {
       x.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
-var findSect = document.querySelectorAll(".sect")
+let findSect = document.querySelectorAll(".sect")
 
 window.addEventListener('scroll', function(evt){
 
